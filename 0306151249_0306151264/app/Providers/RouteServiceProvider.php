b2700e0ea8275787cfb42cd50fa5a5f1eb099cb5
w3leee <33517224+w3leee@users.noreply.github.com>
2018-05-02 16:58:13 +0700

    lu
    1

--- 0306151249_0306151264/app/Providers/RouteServiceProvider.php
+++ 0306151249_0306151264/app/Providers/RouteServiceProvider.php
@@ -7,41 +7,42 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
-     * This namespace is applied to your controller routes.
-     *
-     * In addition, it is set as the URL generator's root namespace.
-     *
-     * @var string
-     */
-    protected $namespace = 'App\Http\Controllers';
+	/**
+	 * This namespace is applied to your controller routes.
+	 *
+	 * In addition, it is set as the URL generator's root namespace.
+	 *
+	 * @var string
+	 */
+	protected $namespace = 'App\Http\Controllers';
 
-    /**
-     * Define your route model bindings, pattern filters, etc.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
+	/**
+	 * Define your route model bindings, pattern filters, etc.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		//
 
-        parent::boot();
-    }
+		parent::boot();
+	}
 
-    /**
-     * Define the routes for the application.
-     *
-     * @return void
-     */
-    public function map()
-    {
-        $this->mapApiRoutes();
+	/**
+	 * Define the routes for the application.
+	 *
+	 * @return void
+	 */
+	public function map()
+	{
+		$this->mapApiRoutes();
 
-        $this->mapWebRoutes();
+		$this->mapWebRoutes();
 
-        //
-    }
+		//
+	}
 
+<<<<<<< HEAD
     /**
      * Define the "web" routes for the application.
      *
@@ -58,19 +59,38 @@ class RouteServiceProvider extends ServiceProvider
              ->namespace($this->namespace)
              ->group(base_path('routes/weblu.php'));
     }
+=======
+	/**
+	 * Define the "web" routes for the application.
+	 *
+	 * These routes all receive session state, CSRF protection, etc.
+	 *
+	 * @return void
+	 */
+	protected function mapWebRoutes()
+	{
+		Route::middleware('web')
+			 ->namespace($this->namespace)
+			 ->group(base_path('routes/web.php'));
+>>>>>>> 651c8f3562cf634ecc36e306cde51b5309296742
 
-    /**
-     * Define the "api" routes for the application.
-     *
-     * These routes are typically stateless.
-     *
-     * @return void
-     */
-    protected function mapApiRoutes()
-    {
-        Route::prefix('api')
-             ->middleware('api')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/api.php'));
-    }
+		Route::middleware('web')
+			 ->namespace($this->namespace)
+			 ->group(base_path('routes/luan.php'));
+	}
+
+	/**
+	 * Define the "api" routes for the application.
+	 *
+	 * These routes are typically stateless.
+	 *
+	 * @return void
+	 */
+	protected function mapApiRoutes()
+	{
+		Route::prefix('api')
+			 ->middleware('api')
+			 ->namespace($this->namespace)
+			 ->group(base_path('routes/api.php'));
+	}
 }
