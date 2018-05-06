8ca40d636dd9b0ba93093fd0ea380a8d0edfe333
Vuminhluan <vuminhluan97@gmail.com>
2018-05-06 21:56:15 +0700

    luân
    chung: sửa css navtop.css. ul thêm class navtop-menu ( ul.navtop-menu )

--- 0306151249_0306151264/resources/views/includes/navtop.blade.php
+++ 0306151249_0306151264/resources/views/includes/navtop.blade.php
@@ -1,20 +1,20 @@
 
-    <!-- nav top -->
+<!-- nav top -->
 <div class="topnavroot">
-<div class="topnav" >
-<ul class="navtop-menu">
-  <li><a class="active" href="#home">Trang chủ</a></li>
-  <li><a href="#news">Thông báo</a></li>
-  <li><a href="#contact">Tin nhắn</a></li>
-  <div  style="float:right;  display: inline-block;" >
-  <div class="search-container">
-    <form action="/action_page.php">
-      <input  type="text" placeholder="Search.." name="search">
-      <button type="submit"><i class="fa fa-search"></i></button>
-    </form>
+  <div class="topnav" >
+    <ul class="navtop-menu">
+      <li><a class="active" href="#home">Trang chủ</a></li>
+      <li><a href="#news">Thông báo</a></li>
+      <li><a href="#contact">Tin nhắn</a></li>
+      <div  style="float:right;  display: inline-block;" >
+        <div class="search-container">
+          <form action="/action_page.php">
+            <input  type="text" placeholder="Search.." name="search">
+            <button type="submit"><i class="fa fa-search"></i></button>
+          </form>
+        </div>
+        <li ><a  href="#about">Người dùng A</a></li>
+      </div>
+    </ul>
   </div>
-  <li ><a  href="#about">Người dùng A</a></li>
-  </div>
-</ul>
-</div>
 </div>
--- 0306151249_0306151264/resources/views/master/luan/trangcanhan_masterpage.blade.php
+++ 0306151249_0306151264/resources/views/master/luan/trangcanhan_masterpage.blade.php
@@ -1,5 +1,9 @@
 @extends('master.masterpage')
 
+@section('css')
+	<link rel="stylesheet" href="{{asset('css/luan/trangcanhan_masterpage.css')}}">
+@endsection
+
 @section('main')
 
 	<div class="profile-image">
@@ -28,6 +32,101 @@
 	</div>
 	<!-- End profile image (banner and avatar) -->
 
-@yield('noidung_trangcanhan')
+
+	<!-- Profile navbar -->
+	<div class="profile-navbar">
+		<div class="main">
+			<ul>
+				<li>
+					<a href="#">
+						<span>Bài viết</span><span>12</span>
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<span>Thích</span><span>0</span>
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<span>Lưu</span><span>0</span>
+					</a>
+				</li>
+				<li>
+					<a href="profile-all-group.html">
+						<span>Nhóm</span><span>2</span>
+					</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<!-- End profile navbar -->
+
+	{{--  body --}}
+	<div class="contentmain main">
+
+		<div class="leftnav-profile-masterpage leftnav-profile sidebar">
+			<div>
+				<div class="profile-name">
+					<h2>Người Dùng A</h2>
+					<p><a href="#">@nguoidunga</a></p>
+
+				</div>
+				<div class="profile-bio">
+					<p>Giới thiệu bản thân ở đây. Tóm tắt khoảng 120 chữ </p>
+				</div>
+				<div class="profile-primary-info">
+					<p>
+						<i class="fa fa-map-marker"></i>
+						&nbsp;
+						<span>Hồ Chí Minh</span>
+					</p>
+					<p>
+						<i class="fa fa-clock-o"></i>
+						&nbsp;
+						<span>Tham gia ngày 04/04/2018</span>
+					</p>
+					<p>
+						<i class="fa fa-calendar"></i>
+						&nbsp;
+						<span>Ngày sinh: 14/07/1997</span>
+					</p>
+				</div>
+				<div class="profile-some-images">
+					<i class="fa fa-image"></i>
+					&nbsp;
+					<a href="#/">Hình ảnh</a>
+					<ul>
+						<li><a href="#/"><img src="{{asset('pictures/luan/test1.png')}}" alt=""></a></li>
+						<li><a href="#/"><img src="{{asset('pictures/luan/test1.png')}}" alt=""></a></li>
+						<li><a href="#/"><img src="{{asset('pictures/luan/test1.png')}}" alt=""></a></li>
+						<li><a href="#/"><img src="{{asset('pictures/luan/test1.png')}}" alt=""></a></li>
+					</ul>
+				</div>
+				<div id="edit-profile-button">
+					<p class="edit-profile edit-name">
+						<i class="fa fa-edit"></i>
+						&nbsp;
+						<a href="#/" onclick="openModal('js-edit-profile-modal')">Chỉnh sửa thông tin hiển thị trang cá nhân.</a>
+					</p>
+				</div>
+			</div>
+		</div>
+		<!--  -->
+		<div class="content">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, veritatis.
+		</div>
+		<!--  -->
+		<div class="rightnav-profile-masterpage sidebar">
+		</div>
+
+	</div>
+	{{--  --}}
+
+
+
+
+
+	@yield('noidung_trangcanhan')
 
 @endsection
