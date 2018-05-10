b0b39545901c2737bba13c65cbcc59e49274d16b
VuMinhLuan <vuminhluan1407@gmail.com>
2018-05-10 19:45:39 +0700

    luân
    contact form

--- 0306151249_0306151264/resources/views/index.blade.php
+++ 0306151249_0306151264/resources/views/index.blade.php
@@ -3,8 +3,8 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Login</title>
-	<link rel="stylesheet" type="text/css" href="css/login.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="{{asset('css/luan/login.css')}}">
+  <link rel="stylesheet" type="text/css" href="{{asset('css/font-awesome.min.css')}}">
 </head>
 <body>
 
--- 0306151249_0306151264/resources/views/khac/lienhe.blade.php
+++ 0306151249_0306151264/resources/views/khac/lienhe.blade.php
@@ -2,45 +2,99 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Login</title>
-	<link rel="stylesheet" type="text/css" href="css/reset-password.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<title>Liên hệ chúng tôi</title>
+  <link rel="stylesheet" href="{{asset('css/luan/contact.css')}}">
+	<link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}">
 </head>
 <body>
+	<div class="background"></div>
+
+	<div class="topnavroot">
+    <div class="topnav wrapper-main">
+      <ul class="navtop-menu">
+        <li><a href="#/">Logo here</a></li>
+        <li><a href="#/">Quên mật khẩu</a></li>
+      </ul>
+    </div>
+  </div>
+
+	<div class=" wrapper-main">
+		<div class="contact-form-container">
+
+	    <div class="contact-form-information">
+	      <div class="title"><h1>Thông tin</h1></div>
+				<div class="info">
+					<ul>
+						<li>
+							<div class="info-icon"><i class="fa fa-map-marker"></i></div>
+
+							<div>67, Huỳnh Thúc Kháng, Phường Bến Nghé, Quận 1, TP. HCM</div>
+						</li>
+						<li>
+							<div class="info-icon"><i class="fa fa-phone"></i></div>
+
+							<div>
+								<p>098-177-0642</p>
+								<p>01634-699-175</p>
+							</div>
+						</li>
+						<li>
+							<div class="info-icon"><i class="fa fa-envelope"></i></div>
+
+							<div>
+								<p>luan@support.com</p>
+								<p>huy@support.com</p>
+							</div>
+						</li>
+					</ul>
+				</div>
+				<div class="social-button">
+					<ul>
+						<li><a href="#/"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="#/"><i class="fa fa-instagram"></i></a></li>
+						<li><a href="#/"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="#/"><i class="fa fa-google-plus"></i></a></li>
+					</ul>
+				</div>
+	    </div>
+
+			{{-- ----------------------------- ----------------------------- --}}
+
+	    <div class="contact-form-main">
+	      <div class="title"><h1>Gửi tin nhắn đến chúng tôi</h1></div>
+				<div>
+					<form id="contact-form" name="contact-form" action="#/" action="POST">
+						<div>
+							<label for="fullname">Họ và tên</label>
+							<input type="text" id="fullname" name="fullname">
+						</div>
+						<div>
+							<label for="email">Email</label>
+							<input type="text" id="email" name="email">
+						</div>
+						<div>
+							<label for="message">Tin nhắn</label>
+							<textarea id="message" name="message" rows="5" cols="80"></textarea>
+						</div>
+						<div>
+							<button class="send-message-button">Gửi</button>
+						</div>
+					</form>
+				</div>
+	    </div>
 
 
-	<div class="reset-password-container">
-		<div> header goes here... maybe</div>
-		<div class="reset-password-main">
-			<div class="reset-password-form">
-				<form action="#/" id="reset-password" name="reset-password" method="POST">
-
-					<div>
-						<h2>Đặt lại mật khẩu</h2>
-					</div>
-
-					<div>
-						<p class="description">Liên lạc</p>
-					</div>
-
-					<div>
-						<label for="reset-password-username">Tên đăng nhập hoặc email</label>
-						<input type="text" id="reset-password-username" required>
-					</div>
-					<div>
-						<button id="reset-passowrd-button">Đặt lại mật khẩu</button>
-					</div>
-				</form>
-			</div>
 		</div>
+  </div>
 
-	</div>
 
 
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.17.0/dist/jquery.validate.min.js"></script>
 
-  <script src="js/login.script.js"></script>
+	<script src="{{asset('js/jquery/jquery3.3.1.js')}}"></script>
+	<script src="{{asset('js/jquery/jquery-validation.min.js')}}"></script>
+
+	<script>
+	</script>
 
 
   <script>
