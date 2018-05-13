fca91e35c9ff3c9fcdd34e8a5c20fe3e1a588786
w3leee <33517224+w3leee@users.noreply.github.com>
2018-05-14 04:34:53 +0700

    lu
    1

--- 0306151249_0306151264/routes/lu.php
+++ 0306151249_0306151264/routes/lu.php
@@ -11,11 +11,11 @@ Route::get("gr/{id}","NhomController\Nhom@loadnhom");
 Route::get("baiviet", function(){
 	return view("includes.baiviet");
 });
-<<<<<<< HEAD
+
 Route::post("/ajax/postbaivietne","BaiVietController\BaiViet@Postbaiviet")->name('postbaiviet');
 Route::get("/ajax/getmabaivietne","BaiVietController\BaiViet@GetMaBaiViet")->name('getmabaiviet');
 Route::get("/ajax/getbaivietne","BaiVietController\BaiViet@GetBaiViet")->name('getbaiviet');
-=======
-Route::post("/hr/postbaivietne","BaiVietController\BaiViet@Dangbaiviet")->name('postbaiviet');
-Route::get("/hr/postbaivietne","BaiVietController\BaiViet@Dangbaiviet")->name('postbaiviet');
->>>>>>> 92c9be281799eb68ede1c0751a28f46ab914153e
+// =======
+// Route::post("/hr/postbaivietne","BaiVietController\BaiViet@Dangbaiviet")->name('postbaiviet');
+// Route::get("/hr/postbaivietne","BaiVietController\BaiViet@Dangbaiviet")->name('postbaiviet');
+
