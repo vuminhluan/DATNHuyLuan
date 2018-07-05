6214678aa6d8fc1e5b5e84befc823493ec557a11
VuMinhLuan <vuminhluan1407@gmail.com>
2018-07-05 20:53:11 +0700

    luân
    footer margin-top 200px

--- 0306151249_0306151264/resources/views/includes/footer.blade.php
+++ 0306151249_0306151264/resources/views/includes/footer.blade.php
@@ -1,3 +1,3 @@
 
 
-<div style="margin-top: 50px; border-width: 1px 0 0px 0; border-style: solid; border-color:#999;height: 200px; line-height: 200px;background-color: #fff; text-align: center;width: 100%;bottom: 0;left: 0;"><p>LL | &copy; 2018</p></div>
+<div style="margin-top: 200px; border-width: 1px 0 0px 0; border-style: solid; border-color:#999;height: 200px; line-height: 200px;background-color: #fff; text-align: center;width: 100%;bottom: 0;left: 0;"><p>LL | &copy; 2018</p></div>
